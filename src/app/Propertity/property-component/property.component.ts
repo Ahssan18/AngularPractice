7f5f9119597ec44267637628547768a3af5746d0
Ahssan18 <ahssanakhtar18@gmail.com>
2022-10-10 02:14:21 +0500

    first commit

---
+++ src/app/Propertity/property-component/property.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component(
+  {
+    selector:'property-card-compoment',
+    templateUrl:'property.Component.html',
+    styleUrls:[ 'property.component.scss']
+  }
+)
+export class PropertyCardComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './Propertity/property-component/property.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
